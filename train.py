eaf3f46dbbdf68c92bcc3297e440b78dc3099354
susowl <gvs1185@gmail.com>
2020-11-18 00:02:13 +0000

    train

--- train.py
+++ train.py
@@ -51,7 +51,7 @@ D_model_A = Discriminator(3).to(device)
 D_model_B = Discriminator(3).to(device)
 PrintModelsParameters(generator=G_model_AtoB, discriminator=D_model_A)
 
-def save_model(model, mode="AtoB"):
+def save_model(model, dataset=args.dataset, tb=args.tb, mode="AtoB"):
     directory = "ignore/weigths/%s" % args.dataset
     try:
         if not os.path.exists(directory):
@@ -59,7 +59,7 @@ def save_model(model, mode="AtoB"):
             print("Create directory: " + directory)
     except OSError:
         print ('Error: Creating directory. ' +  directory)
-    torch.save(model.state_dict(), "ignore/weights/%s/%s_s.pth" % (args.dataset, args.tb, mode))
+    torch.save(model.state_dict(), "ignore/weights/%s/%s_s.pth" % (dataset, tb, mode))
 
 # init optimizer and scheduler
 print(" "*75, "\r", "Loading optimizer...", end="\r")
@@ -72,7 +72,7 @@ lr_scheduler_D_A = torch.optim.lr_scheduler.LambdaLR(optimizer_D_A, lr_lambda=La
 lr_scheduler_D_B = torch.optim.lr_scheduler.LambdaLR(optimizer_D_B, lr_lambda=LambdaLR(args.epochs, 0, 100).step)
 
 
-def train():
+def train(args.dataset, args.tb):
     print(" "*75, "\r", "Loading training...", end="\r")
     logger = Logger(dataset=args.dataset, tb=args.tb, batch_size=args.batch,
                     vis=args.vis ,len_data=len(train_loader))
@@ -126,8 +126,8 @@ def train():
             logger.printer(iter=1)
             logger.visual(img_A, img_B, same_A, same_B, fake_A, fake_B, res_A, res_B,
                           iter=50)
-        save_model(G_model_AtoB, mode="AtoB")
-        save_model(G_model_BtoA, mode="BtoA")
+        save_model(G_model_AtoB, dataset=args.dataset, tb=args.tb, mode="AtoB")
+        save_model(G_model_BtoA, dataset=args.dataset, tb=args.tb, mode="BtoA")
         logger.printer_epoch()
         logger.tensorboard_epoch(writer=writer)
 
